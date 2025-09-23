b60862059f8352ff7c3b066b43e346dce1207169
Bart Stoffels <bart.stoffels@student.gildeopleidingen.nl>
2025-09-23 10:33:16 +0200

    composer toegevoegd zodat alle klassen met de namespace App te vinden zijn in de src/ map

--- site/database/db.php
+++ site/database/db.php
@@ -1,21 +1,29 @@
-<!-- db verbinding -->
 <?php
-$host = '127.0.0.1'; // of 'localhost'
-$db   = 'datacenter_db'; // De naam van je database
-$user = 'jouw_gebruikersnaam';
-$pass = 'jouw_wachtwoord';
-$charset = 'utf8mb4';
+class Database {
+    private $host = '127.0.0.1';
+    private $db   = 'datacenter_db';
+    private $user = 'jouw_gebruikersnaam';
+    private $pass = 'jouw_wachtwoord';
+    private $charset = 'utf8mb4';
+    private $pdo;
 
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-];
+    public function __construct() {
+        $dsn = "mysql:host={$this->host};dbname={$this->db};charset={$this->charset}";
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
 
-try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
-} catch (\PDOException $e) {
-    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        try {
+            $this->pdo = new PDO($dsn, $this->user, $this->pass, $options);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+
+    public function getConnection() {
+        return $this->pdo;
+    }
 }
 ?>
