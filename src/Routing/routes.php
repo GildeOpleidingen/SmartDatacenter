b1e30a4c72d260d0bc6adc3fc4567beefff4bf1a
Larsie2 <larsrutjens1@gmail.com>
2026-03-23 09:44:43 +0100

    added api-routes and deleted SettingsController

--- src/Routing/routes.php
+++ src/Routing/routes.php
@@ -1,13 +1,15 @@
 <?php
 
 use App\Controllers\DashboardController;
-use App\Controllers\SettingsController;
 use App\Controllers\WebhookController;
 
+// WEB ROUTES
 $router->get('/', DashboardController::class, 'index');
 $router->get('/dashboard', DashboardController::class, 'index');
 
-$router->get('/settings', SettingsController::class, 'index');
-$router->post('/settings', SettingsController::class, 'updateSettings');
+// API ROUTES
+$router->get('/api/status', WebhookController::class, 'status');
 
 $router->post('/api/webhooks/ttn', WebhookController::class, 'handle');
+
+$router->post('/api/downlink/light', WebhookController::class, 'busylight');
