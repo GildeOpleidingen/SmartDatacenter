0d714c5c074bdcbd12c9b39c2f47b5d56b9c8011
Larsie2 <larsrutjens1@gmail.com>
2026-03-26 09:49:30 +0100

    Webhook handling, filename correction, namespaces implemented

--- src/Controllers/WebhookController.php
+++ src/Controllers/WebhookController.php
@@ -1,6 +1,22 @@
 <?php
 namespace App\Controllers;
 
+use App\Services\DBParser;
+
 class WebhookController {
-    
+    public function handle() {
+        $json = file_get_contents('php://input');
+        $data = json_decode($json, true);
+        
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            http_response_code(400);
+            exit(json_last_error_msg());
+        }
+
+        $parser = new DBParser();
+        $parser->parseDataToDB($data);
+
+        http_response_code(200);
+        echo json_encode(["status" => "received"]);
+    }
 }
