bb0d9798674a5ec554fdf0a5174c4a419d0505d5
EbubekirBozok <EbubekirBozok1@gmail.com>
2026-04-16 13:10:56 +0200

    Website verbetered

--- views/Dashboard.php
+++ views/Dashboard.php
@@ -13,7 +13,7 @@
 <body class="text-white p-10">
 <div class="body_wrapper">
     <div class="main_navigation">
-        <section>
+        <section style="padding: 1rem 1.5rem 1rem 1.5rem;  align-items: center;">
                 <div class="container">
                      <!-- Header -->
                      <div class="flex flex-col mb-10">
@@ -23,32 +23,34 @@
             </section>
     </div>
     <div class="main_wrapper">
-        <section style="padding: 120px">
-            <div class="max-w-screen-xl container">
+        <section style="padding: 120px; align-items: center;">
+            <div class="container max-w-screen-xl">
               <!--  <div class="grid grid-cols-8 md:grid-cols-2 gap-10 max-w-fit"> -->
-                <div class="inline-grid grid-cols-8 md:grid-cols-2 gap-10">
+                <div class="inline-grid max-lg:grid-cols-3 lg:grid-cols-4 md:grid-cols-2 gap-10">
                     <?php
                     foreach ($deviceCards as $card):
                         [$borderColor, $statusLabel] = $definer->status_color_and_label($card->status);  /* Will implement status later */
                         ?>
-                        <div class="flex-row rounded-xl border-2 overflow-hidden p-12" style="border-color: <?= $borderColor ?>"> <!-- card -->
+                        <div class="flex flex-col justify-between items-center rounded-xl border-2 overflow-hidden p-8 h-full" style="border-color: <?= $borderColor ?>"> <!-- card -->
                                 <!-- <div class="text-right">
                                     div class="font-semibold"><//?= $statusLabel ?></div
                                 </div> -->
-                                <ul class="flex flex-col items-center justify-center bg-[#001] overflow-hidden"> <!-- card-inner -->
-                                    <div class="w-10 block h-auto">
-                                    <?php
-                                    //echo $card->DOOR_OPEN_STATUS;
-                                    echo $deviceIcon = $definer->getDeviceIcon($card->sensorType, $card->status, isset($card->DOOR_OPEN_STATUS) ? $card->DOOR_OPEN_STATUS : null, isset($card->motion) ? $card->motion : null, isset($card->state) ? $card->state : null) ?? "<img src='/img/sensors/default/GildeDataCenterIconGood.svg' width=\"64px\" class=\"ml-auto\">";
-                                    ?>
-                                    </div>
+                            <div class="w-11 block h-auto">
+                                <?php
+                                //echo $card->DOOR_OPEN_STATUS;
+                                echo $deviceIcon = $definer->getDeviceIcon($card->sensorType, $card->status, isset($card->DOOR_OPEN_STATUS) ? $card->DOOR_OPEN_STATUS : null, isset($card->motion) ? $card->motion : null, isset($card->state) ? $card->state : null) ?? "<img src='/img/sensors/default/GildeDataCenterIconGood.svg' width=\"64px\" class=\"ml-auto\">";
+                                ?>
+                            </div>
+                            <div class="flex-grow"></div>
+                            <ul class="flex flex-col items-center justify-center bg-[#001] overflow-hidden w-full"> <!-- card-inner -->
+
                                     <p class="text-lg font-semibold"><?= htmlspecialchars($card->device_id) ?></p>
                                     <?php foreach ($card as $key => $value):
                                         if ($key === "device_id" || $key === "sensorType" || $key === "button" || $key === "MOD" || $key === "temperature" || $key === "humi" || $key === "status") continue;
                                         ?>
-                                        <li class="grid grid-cols-2 gap-1">
-                                            <span class="text-gray-300 text-right"><?= $definer->getLabels($key) ?></span>
-                                            <span class="font-medium">= <?= htmlspecialchars($value) ?></span>
+                                        <li class="grid grid-cols-2 gap-1 w-full">
+                                            <span class="text-gray-300 text-right break-all text-xs"><?= $definer->getLabels($key) ?></span>
+                                            <span class="font-medium break-all  text-xs">= <?= htmlspecialchars($value) ?></span>
                                         </li>
                                     <?php endforeach; ?>
                                 </ul>
